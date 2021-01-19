91fd7cbc0aaa87fd2a30a212cf5524c7ae912f45
AlexEpu <65967586+AlexEpu@users.noreply.github.com>
2021-01-19 19:32:17 +0200

    Almost Done

---
+++ src/components/ResultCard.js
@@ -0,0 +1,70 @@
+/** @format */
+
+import React, { useContext } from "react";
+import Moment from "react-moment";
+import { GlobalContext } from "../context/GlobalState";
+
+export const ResultCard = ({ movie }) => {
+  const {
+    addMovieToWatchlist,
+    addMovieToWatched,
+    watchlist,
+    watched,
+  } = useContext(GlobalContext);
+
+  let storedMovie = watchlist.find((o) => o.id === movie.id);
+  let storedMovieWatched = watched.find((o) => o.id === movie.id);
+
+  const watchlistDisabled = storedMovie
+    ? true
+    : storedMovieWatched
+    ? true
+    : false;
+
+  const watchedDisabled = storedMovieWatched ? true : false;
+
+  return (
+    <div className="result-card">
+      <div className="poster-wrapper">
+        {movie.poster_path ? (
+          <img
+            src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+            alt={`${movie.title} Poster`}
+          />
+        ) : (
+          <div className="filler-poster" />
+        )}
+      </div>
+
+      <div className="info">
+        <div className="header">
+          <h3 className="title">{movie.title}</h3>
+          <h4 className="release-date">
+            <Moment format="YYYY">{movie.release_date}</Moment>
+          </h4>
+          <h6 className="movie-plot">{movie.overview}</h6>
+        </div>
+
+        <div className="controls">
+          <button
+            id="watch-btn"
+            className="btn btn-watch"
+            disabled={watchlistDisabled}
+            onClick={() => addMovieToWatchlist(movie)}
+          >
+            Add to Watchlist
+          </button>
+
+          <button
+            id="watched-btn"
+            className="btn btn-watched"
+            disabled={watchedDisabled}
+            onClick={() => addMovieToWatched(movie)}
+          >
+            Add to Watched
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
